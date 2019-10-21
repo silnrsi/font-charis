2338874125e843fed8e919a30b0f75ed8987f391
nrsiward <nrsiward@users.noreply.github.com>
2019-10-21 15:29:01 -0500

    Normalized test file names [nobuild]

--- tools/genftml_CR.bat
+++ tools/genftml_CR.bat
@@ -2,7 +2,7 @@ cd C:\Users\wardak\Mine\smith_vm\font-charis
 
 py C:\Users\wardak\Mine\smith_vm\font-charis\tools\psfgenftml.py "source\CharisSIL-Regular.ufo" "tests\AllCharsCR.ftml" -t allchars -f C -i "source\glyph_data.csv" -s "../results/CharisSIL-Regular.ttf" --scale 200 -l "tests\logs\AllChars_CR.log"
 
-py C:\Users\wardak\Mine\smith_vm\font-charis\tools\psfgenftml.py "source\CharisSIL-Regular.ufo" "tests\DiacsCR.ftml" -t diac -f C -i "source\glyph_data.csv" -s "../results/CharisSIL-Regular.ttf" --scale 200 -l "tests\logs\Diacs_CR.log"
+py C:\Users\wardak\Mine\smith_vm\font-charis\tools\psfgenftml.py "source\CharisSIL-Regular.ufo" "tests\DiacsCR.ftml" -t diac -f C -i "source\glyph_data.csv" -s "../results/CharisSIL-Regular.ttf" --scale 200 -l "tests\logs\Diac_CR.log"
 
 py C:\Users\wardak\Mine\smith_vm\font-charis\tools\psfgenftml.py "source\CharisSIL-Regular.ufo" "tests\FeaturesCR.ftml" -t features -f C -i "source\glyph_data.csv" -s "../results/CharisSIL-Regular.ttf" --scale 200 -l "tests\logs\Features_CR.log"
 
