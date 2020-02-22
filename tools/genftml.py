2efcbda0c270dce2ac51b78e769f76a93122729c
nrsiward <nrsiward@users.noreply.github.com>
2020-02-21 18:30:19 -0600

    Finished genftml.py and tested against Charis [nobuild]

--- tools/genftml.py
+++ tools/genftml.py
@@ -1,47 +1,61 @@
 # Script to call psfgenftml.py multiple times -- once for each test
-# Relies on "smith alltest" to generate html that will invoke the generated ftml for each style
+# Relies on "smith test" to generate html that will render the generated ftml for each style
 
-import sys
+import sys, glob, os.path
 import psfgenftml
 
-test_lst = [
-    "allchars",
-    "diacs",
-    "features",
-    "smcp"
-]
-
+# tests to generate (see psfgenftml.py)
+test_lst = ["allchars", "diacs", "features", "smcp"]
 AP_type_lst = ["U", "L", "O", "H", "R"]
 for a in AP_type_lst:
     test_lst.append("features_" + a)
     test_lst.append("smcp_" + a)
 
-# Paths are relative to directory script is ran from (font-<>/tools)
-# Assumes the standard directory structure is present
+# find file name without extension of the Regular style ufo
+regular_ufo_fn_lst = glob.glob("source/*-Regular.ufo")
+regular_ufo_base = os.path.splitext(os.path.basename(regular_ufo_fn_lst[0]))[0]
 
-ufo_regular = "CharisSIL-Regular.ufo"
-test_nm = "allchars"
-ftml_fn = test_nm + ".ftml"
-font_spec = "C"
-glyph_data_fn = "glyph_data.csv"
-xsl_fontsrc = "CharisSIL-Regular.ttf"
-ftml_scale = "200"
-log_fn = test_nm + ".log"
+# values to be applied to items in arg_template_lst
+arg_values_dict = {
+    "test" : test_lst[0],
+    "ufo_regular" : regular_ufo_base,
+    "font_code" : regular_ufo_base[0],
+    "glyph_data": "glyph_data",
+    "scale" : "200",
+}
 
-args_lst = [
-"../source/CharisSIL-Regular.ufo",
-"../tests/AllCharsCR.ftml",
-"-t", "allchars",
-"-f", "C",
-"-i", "../source/glyph_data.csv",
-"-s", "../results/CharisSIL-Regular.ttf",
-"--scale", "200",
-"-l", "../tests/logs/AllChars_CR.log"
+# This arg_lst can be used for debugging
+#  but will be replaced by "instances" of arg_template_lst (below)
+arg_lst = [
+    "source/CharisSIL-Regular.ufo",
+    "tests/allchars.ftml",
+    "-t", "allchars",
+    "-f", "C",
+    "-i", "source/glyph_data.csv",
+    "-s", "results/CharisSIL-Regular.ttf",
+    "--scale", "200",
+    "-l", "tests/logs/allchars.log",
 ]
 
-print(sys.argv)
-print(psfgenftml.__file__)
-sys.argv = [psfgenftml.__file__]
-sys.argv.extend(args_lst)
+# Templates for arguments to be passed to psfgenftml.py thru sys.argv
+# Paths are relative to the directory the script is ran from
+#  which is assumed to be the top of the font repo
+# Assumes the standard directory structure is present
+arg_template_lst = [
+    "source/{ufo_regular}.ufo",
+    "tests/{test}.ftml",
+    "-t", "{test}",
+    "-f", "{font_code}",
+    "-i", "source/{glyph_data}.csv",
+    "-s", "results/{ufo_regular}.ttf",
+    "--scale", "{scale}",
+    "-l", "tests/logs/{test}.log",
+]
 
-psfgenftml.cmd()
+# Call psfgenftml for each test
+for test in test_lst:
+    arg_values_dict["test"] = test
+    arg_lst = [arg.format(**arg_values_dict) for arg in arg_template_lst]
+    sys.argv = [psfgenftml.__file__]
+    sys.argv.extend(arg_lst)
+    psfgenftml.cmd()
