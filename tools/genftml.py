257e14dea45f1a854c08d4a43d742f867a8416f9
nrsiward <nrsiward@users.noreply.github.com>
2024-08-24 17:08:06 -0500

    Modify fmtl generation so reference fonts are specified in a separate file Update reference fonts used [nobuild]

--- tools/genftml.py
+++ tools/genftml.py
@@ -104,11 +104,10 @@ for test in test_lst:
         style = fn[fn.find('-') + 1]
         style += 'I' if re.search("-.+Italic", fn) else ''
         arg_lst.extend(["-s", "../results/{}={}{}".format(fn, family, style)])
-    # TODO: kludgy way to add columns for other fonts, assumes tests/reference folder
-    arg_lst.extend(["-s", "../references/v6101/CharisSIL-Regular.ttf=CR6"])
-    arg_lst.extend(["-s", "../references/v6101/CharisSIL-Bold.ttf=CB6"])
-    arg_lst.extend(["-s", "../references/v6101/CharisSIL-Italic.ttf=CI6"])
-    arg_lst.extend(["-s", "../references/v6101/CharisSIL-BoldItalic.ttf=CBI6"])
+    # add columns for reference fonts
+    from ref_fonts_ftml import ref_fonts
+    for ref_font in ref_fonts:
+        arg_lst.extend(["-s", ref_font])
     sys.argv = [psfgenftml.__file__]
     sys.argv.extend(arg_lst)
     psfgenftml.cmd()
---
+++ tools/ref_fonts_ftml.py
@@ -0,0 +1,6 @@
+ref_fonts = [
+	"../references/v6200/CharisSIL-Regular.ttf=CR6",
+	"../references/v6200/CharisSIL-Bold.ttf=CB6",
+    "../references/v6200/CharisSIL-Italic.ttf=CI6",
+    "../references/v6200/CharisSIL-BoldItalic.ttf=CBI6",
+]
